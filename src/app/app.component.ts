b2f5e73f775e66b7d8c67d3564c85b5801e0ce57
rajendra <razendhraprasadreddyn@gmail.com>
2017-06-27 16:11:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  
+  clickMessage : string = '';
+  values1 : string = '';
+  values2 : string = '';
+  fieldValue : string = '';
+  enteredValue :string = '';
+  values : any[] = ['apple','orange','banana','mango']
+  
+  
+  onClickMe(): void
+  {
+    this.clickMessage = "my page event";
+  }
+  onKey(event: any) : void
+  {
+  debugger;
+    this.values1 += event.target.value;
+  }
+   
+  onEnter(value: string) { 
+  this.enteredValue = value; }
+  
+  update(event : string): void
+  {
+    this.fieldValue = event;
+  }
+  
+  addItem(event : string) : void
+  {
+    if(event)
+    {
+    alert(event);
+    alert(this.values);
+    this.values.push(event);
+    debugger;
+    }
+  }
 }
